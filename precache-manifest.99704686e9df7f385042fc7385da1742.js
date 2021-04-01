54d7d194a4b81ef508380675505bfe0d174a0bd6
mattj299 <mateonas586@gmail.com>
2021-04-01 14:06:10 -0700

    Updates

---
+++ precache-manifest.99704686e9df7f385042fc7385da1742.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6ab916ed0cf1e5a935a4e61c5db0f5b",
+    "url": "/react-quiz-opentdb-api/index.html"
+  },
+  {
+    "revision": "70902442d0150ccec001",
+    "url": "/react-quiz-opentdb-api/static/css/main.2eaf904b.chunk.css"
+  },
+  {
+    "revision": "50b8f867192aac990a7c",
+    "url": "/react-quiz-opentdb-api/static/js/2.f838f255.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-quiz-opentdb-api/static/js/2.f838f255.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70902442d0150ccec001",
+    "url": "/react-quiz-opentdb-api/static/js/main.d136ae46.chunk.js"
+  },
+  {
+    "revision": "d99a50a9ffe45693e589",
+    "url": "/react-quiz-opentdb-api/static/js/runtime-main.b0f6dbcf.js"
+  }
+]);
